ac37489ef4702bf5ba7b265770e0c268554f3677
Alexey Zaslavsky <pterolex@gmail.com>
2019-02-10 19:33:23 +0200

    Add basic commands for creating and updating UI elements

--- client.js
+++ client.js
@@ -3,17 +3,70 @@ const WebSocket = require('ws');
 const ws = new WebSocket('ws://localhost:9000');
 
 ws.on('open', function open() {
-    ws.send('something');
+    // ws.send('something');
 });
 
 ws.on('message', function incoming(data) {
     console.log(data);
 });
 
+const getCommands = () => (
+    [
+        {
+            id: 1,
+            elementId: 2,
+            command: "create_element",
+            args: [
+                "button"
+            ]
+        },
+        {
+            id: 2,
+            elementId: 2,
+            command: "set_label",
+            args: [
+                new Date().toString()
+            ]
+        },
+        // {
+        //     id: 1,
+        //     elementId: 2,
+        //     command: "create_element",
+        //     args: [
+        //         "button",
+        //         "Click me!"
+        //     ]
+        // },
+        // {
+        //     id: 2,
+        //     elementId: 2,
+        //     command: "set_label",
+        //     args: [
+        //         new Date().toString()
+        //     ]
+        // },
+        {
+            id: 5,
+            elementId: 5,
+            command: "set_label",
+            args: [
+                new Date().toString()
+            ]
+        }
+    ]
+)
+
+let counter = 0;
+
 setInterval(() => {
     console.log('Sending data...');
 
-    ws.send(new Date().toString())
-}, 10000);
+    const commands = getCommands();
+    const command = commands[counter % commands.length];
+
+    ws.send(JSON.stringify(command));
+
+    counter++;
+}, 5000);
 
 
