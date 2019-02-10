218971b7e796f720aa092e5521f3c5fc7317bf1b
Alexey Zaslavsky <pterolex@gmail.com>
2019-02-10 21:40:02 +0200

    Implement counter

--- client.js
+++ client.js
@@ -2,71 +2,141 @@ const WebSocket = require('ws');
 
 const ws = new WebSocket('ws://localhost:9000');
 
+let counter = 0;
+
+const DECREASE_BUTTON = 2;
+const INCREASE_BUTTON = 3;
+const LABEL_ID = 4;
+
+const getChangeCounterCommand = (counter) => (
+    {
+        id: 2,
+        elementId: 4,
+        command: "set_label",
+        args: [
+            counter.toString()
+        ]
+    }
+)
+
+const sendCommand = (command) => {
+    console.log('Sending command');
+    ws.send(JSON.stringify(command));
+}
+
 ws.on('open', function open() {
     // ws.send('something');
 });
 
-ws.on('message', function incoming(data) {
-    console.log(data);
+ws.on('message', function incoming(dataString) {
+    console.log('message', dataString);
+
+    try {
+        const data = JSON.parse(dataString);
+        console.log(data);
+
+        const elementId = parseInt(data.elementId);
+        const isIncrease = elementId === INCREASE_BUTTON;
+        const isDecrease = elementId === DECREASE_BUTTON;
+
+        if (!isIncrease && !isDecrease) {
+            return;
+        }
+
+        console.log('isIncrease', isIncrease);
+        console.log('isDecrease', isDecrease);
+        console.log('elementId', elementId);
+        counter += isDecrease ? -1 : +1;
+        const command = getChangeCounterCommand(counter);
+
+        sendCommand(command);
+    } catch(e) {
+        console.error('JSON parse error!', e);
+    }
 });
+
+
 
 const getCommands = () => (
     [
         {
             id: 1,
-            elementId: 2,
+            elementId: DECREASE_BUTTON,
             command: "create_element",
             args: [
                 "button"
             ]
         },
         {
-            id: 2,
-            elementId: 2,
-            command: "set_label",
+            id: 1,
+            elementId: LABEL_ID,
+            command: "create_element",
             args: [
-                new Date().toString()
+                "label"
             ]
         },
-        // {
-        //     id: 1,
-        //     elementId: 2,
-        //     command: "create_element",
-        //     args: [
-        //         "button",
-        //         "Click me!"
-        //     ]
-        // },
-        // {
-        //     id: 2,
-        //     elementId: 2,
-        //     command: "set_label",
-        //     args: [
-        //         new Date().toString()
-        //     ]
-        // },
+        {
+            id: 1,
+            elementId: INCREASE_BUTTON,
+            command: "create_element",
+            args: [
+                "button"
+            ]
+        },
         {
             id: 5,
             elementId: 5,
+            command: "create_element",
+            args: [
+                "progress"
+            ]
+        },
+        {
+            id: 5,
+            elementId: 5,
+            command: "set_fraction",
+            args: [
+                0.6
+            ]
+        },
+        {
+            id: 2,
+            elementId: INCREASE_BUTTON,
             command: "set_label",
             args: [
-                new Date().toString()
+                "Increase counter"
+            ]
+        },
+        {
+            id: 2,
+            elementId: DECREASE_BUTTON,
+            command: "set_label",
+            args: [
+                "Decrease counter"
+            ]
+        },
+        {
+            id: 3,
+            elementId: LABEL_ID,
+            command: "set_label",
+            args: [
+                "0"
             ]
         }
     ]
 )
 
-let counter = 0;
+let commandCounter = 0;
 
 setInterval(() => {
-    console.log('Sending data...');
-
     const commands = getCommands();
-    const command = commands[counter % commands.length];
+    const command = commands[commandCounter];
 
-    ws.send(JSON.stringify(command));
+    if (command) {
+        sendCommand(command)
+    }
 
-    counter++;
-}, 5000);
+    commandCounter++;
+}, 10);
 
 
